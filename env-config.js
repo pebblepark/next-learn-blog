82d9a74bca87a7a4ae719ac0eceb2aaf601de62a
pebblepark <subling02@gmail.com>
2022-07-08 17:15:38 +0900

    Fix: babel 설정 제거

--- env-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  'process.env.RESOURCE_URL':
-    process.env.NODE_ENV === 'production'
-      ? 'https://pebblepark.github.io/next-learn-blog'
-      : '',
-};
--- next.config.js
+++ next.config.js
@@ -10,17 +10,4 @@ module.exports = {
         ? 'https://pebblepark.github.io/next-learn-blog/'
         : '/',
   },
-  webpack: (config, { dev }) => {
-    // Perform customizations to webpack config
-    // console.log('webpack');
-    // console.log(config.module.rules, dev);
-    config.module.rules = config.module.rules.map((rule) => {
-      if (rule.loader === 'babel-loader') {
-        rule.options.cacheDirectory = false;
-      }
-      return rule;
-    });
-    // Important: return the modified config
-    return config;
-  },
 };
